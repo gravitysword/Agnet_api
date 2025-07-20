55870b0639995176c52bf1bf4dba76f3fe453ebc
gravitysword <147303481+gravitysword@users.noreply.github.com>
2025-07-20 16:46:03 +0800

    Update login.py

--- login.py
+++ login.py
@@ -4,10 +4,10 @@ a = [ DoubaoAgent,KimiAgent,QwenAgent, MinimaxAgent,ChatglmAgent,DeepseekAgent]
 
 
 
-async def init_agent():
+async def login():
     for i in a:
         await i("","").login()
 
 if __name__ == '__main__':
     import asyncio
-    asyncio.run(init_agent())
+    asyncio.run(login())
